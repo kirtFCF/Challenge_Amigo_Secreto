90978137b6451a0a2a6f62fc748948370025e101
Fariki <77152395@gmail.com>
2025-08-01 16:27:57 -0500

    version final del challenge amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let contador = 1;
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value.trim();
+    if (nombre === ""){
+        alert("Por favor, ingresa un nombre.");
+    } else if (listaAmigos.includes(nombre)) {
+        alert("El nombre ya está en la lista.");
+    } else{
+        listaAmigos.push(nombre);
+    }
+    console.log(listaAmigos);
+    mostrarLista();
+    limpiarLista();
+}
+
+function limpiarLista(){
+    document.querySelector("#amigo").value = "";
+}
+function mostrarLista(){
+    alert(`lista de amigos: ${listaAmigos.join(", ")}`);
+    console.log(`lista de amigos: ${listaAmigos.join(", ")}`);
+}
+
+function sortearAmigo(){
+    if (listaAmigos.length === 0){
+        alert("no hay amigos para sortear");
+    }else{
+        let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+        let amigoSeleccionado = listaAmigos[indiceAleatorio];
+        alert(`El amigo seleccionado es: ${amigoSeleccionado}, Usted a sorteado ${contador++} ${contador === 1 ? 'vez' : 'veces'}`);
+    }
+}
+function cantidadDeSorteos(){
+    if (listaAmigos.length === 0){
+        alert("no hay amigos para sortear");
+    }else{
+        alert(`Cantidad de sorteos realizados: ${contador}`);
+    }
+}
+
